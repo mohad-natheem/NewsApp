9af1561bf5bbe6f4e9f90befdc84bc8b5b329137
Natheem <natheem2580@gmail.com>
2024-02-20 22:38:25 +0530

    initial commit

---
+++ app/src/main/java/com/loc/newsapp/presentation/onboarding/components/OnBoardingPage.kt
@@ -0,0 +1,52 @@
+package com.loc.newsapp.presentation.onboarding.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import com.loc.newsapp.R
+import com.loc.newsapp.presentation.Dimens.MediumPadding1
+import com.loc.newsapp.presentation.Dimens.MediumPadding2
+import com.loc.newsapp.presentation.onboarding.Page
+
+@Composable
+fun OnBoardingPage(
+    modifier : Modifier = Modifier,
+    page : Page
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(id = page.image),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.6f),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.height(MediumPadding1))
+        Text(
+            modifier = Modifier.padding(horizontal = MediumPadding2),
+            text = page.title,
+            style = MaterialTheme.typography.displaySmall.copy(fontWeight = FontWeight.Bold),
+            color = colorResource(id = R.color.display_small)
+        )
+        Text(text = page.description, style = MaterialTheme.typography.bodyMedium, color = colorResource(
+            id =  
+        ))
+
+    }
+    
+}
